c1a0347433189cf04a162f1ab72f294331bba84f
R1PLI <treadz11@gmail.com>
2017-10-02 17:08:41 +0300

    Kotlin: randomizer for rectangle

--- src/main/kotlin/Rectangle.kt
+++ src/main/kotlin/Rectangle.kt
@@ -1,5 +1,19 @@
+import java.util.*
 
-class Rectangle(private val height: Int, private val width: Int)
-{ val isSquare: Boolean
-    get() = height == width
+class Rectangle(private val height: Int, private val width: Int) {
+    val isSquare: Boolean
+        get() = height == width
+
+    override fun toString(): String {
+        return "Rectangle(height=$height, width=$width)"
+    }
+
+}
+
+fun createRandomRectangle(): Rectangle {
+    val random = Random()
+    return Rectangle(
+            height = random.nextInt(),
+            width = random.nextInt()
+    )
 }
