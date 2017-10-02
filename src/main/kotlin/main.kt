3d977aa071c1f24f57301a752a19dfe07ece45e6
R1PLI <treadz11@gmail.com>
2017-10-02 15:29:35 +0300

    Kotlin: basic stuff, part 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,17 @@
 fun main(args: Array<String>) {
     val persons = listOf(Person("Alice", 20), Person("Bob", 21))
-    println(findMaxInList(persons))
+    println(findPersonWithMaxAge(persons))
+    println("The biggest age times 2 is ${findBiggestAgeTimesByValue(persons, 2)}")
     printWithUnitFun("Oleksandr")
     println(asList(1, 2, 3))
     println(getSumInList(1, 2, 3, 4))
 }
 
-fun findMaxInList(list: List<Person>): Person? = list.maxBy { it.age ?: 0 }
+fun findPersonWithMaxAge(list: List<Person>): Person? = list.maxBy { it.age ?: 0 }
+
+fun findBiggestAgeTimesByValue(list: List<Person>, multiply: Int): Int? {
+    return list.map { it.age!! * multiply }.maxBy { it }
+}
 
 fun printWithUnitFun(name: String?) = println("Hi, my name is $name")
 
