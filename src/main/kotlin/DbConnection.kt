880b214e6a28dbec0919b91e0e6d8ffef3da5432
Oleksandr <Oleksandr_Pochernin@epam.com>
2018-01-26 16:36:07 +0200

    [OP] Few 'READ' methods, added comments to methods, adds extension methods to avoid `` problem

--- src/main/kotlin/DbConnection.kt
+++ src/main/kotlin/DbConnection.kt
@@ -1,17 +1,19 @@
 import com.mongodb.*
 
 fun main(args: Array<String>) {
+    //create connection and return collection from database
     val mongoCollection = setupMongoConnection("localhost", 27017)
-            .getMongoDb("api-testing")
+            .getDB("api-testing")
             .getCollection("requests")
 
+    //insert element into db
     mongoCollection.insertElements(
             createMongoObject(
                     mapOf(
                             "id" to "18.0",
                             "title" to "faust1",
                             "author" to "Gete2",
-                            "array" to arrayOf(1, 2, 3),
+                            "array" to arrayOf(1, createMongoObject(mapOf("object id inside of array" to "20")), 3),
                             "new Object" to createMongoObject(
                                     mapOf(
                                             "inside object id" to "20",
@@ -22,23 +24,61 @@ fun main(args: Array<String>) {
             )
     )
 
-    //get element using map
-    val cursorMap = mongoCollection.getDBCursor(mapOf("array" to arrayOf(1, 2, 3)))
+    //create db cursor with use of Map
+    val cursorMap = mongoCollection.getDBCursor(mapOf("array" to arrayOf(1, createMongoObject(mapOf("object id inside of array" to "20")), 3)))
 
-    //get element using object
-    val obj = createMongoObject(mapOf("array" to arrayOf(1, 2, 3)))
-    val cursorObject = mongoCollection
-            .getDBCursor(obj)
+    //create db cursor using createMongoObject method
+    val cursorObject = mongoCollection.getDBCursor(createMongoObject(mapOf("no" to "no")))
 
+    //tries to get object from db, gets error message
+    println(cursorObject?.getDBObject() ?: "No such object in db")
+
+    //tries to get object from db, gets object
+    println(cursorMap?.getDBObject()?.get("array") ?: "No such object in db")
+
+    //tries to get element from object in db, gets elements
+    println(cursorMap?.getElement("array") ?: "No such element in database")
+
+    //tries to get all elements in collection, gets elements collections
+    cursorMap?.getAllElements()?.forEach { println(it) }
+
+    //tries to get limited number of elements in collection, gets elements with fixed size
+    cursorMap?.getAllElementsWithLimit(4)?.forEach { println(it) }
 }
 
 private fun setupMongoConnection(host: String, port: Int): MongoClient = MongoClient(host, port)
 
-private fun MongoClient.getMongoDb(dbname: String): DB = getDB(dbname)
-
 fun DBCollection.getDBCursor(query: BasicDBObject): DBCursor? = this.find(query)
 
 fun <T> DBCollection.getDBCursor(properties: Map<String, T>): DBCursor? = this.find(createMongoObject(properties))
+
+fun DBCursor.getDBObject(): DBObject? {
+    return this.one()
+}
+
+fun DBCursor.getElement(parameter: String): Any? {
+    return this.one().get(parameter)
+}
+
+fun DBCursor.getAllElements(): List<DBObject>? {
+    var elements = emptyList<DBObject>()
+    this.use {
+        while (this.hasNext()) {
+            elements += this.next()
+        }
+    }
+    return elements
+}
+
+fun DBCursor.getAllElementsWithLimit(limit: Int): List<DBObject>? {
+    var elements = emptyList<DBObject>()
+    this.use {
+        while (this.hasNext() && elements.size < limit) {
+            elements += this.next()
+        }
+    }
+    return elements
+}
 
 fun DBCollection.insertElements(elements: BasicDBObject): WriteResult = this.insert(elements)
 
--- src/main/kotlin/JongoNoSQL.kt
+++ src/main/kotlin/JongoNoSQL.kt
@@ -1,11 +1,19 @@
 import com.mongodb.MongoClient
+import org.jongo.Find
+import org.jongo.FindOne
 import org.jongo.Jongo
+import org.jongo.MongoCursor
 
 
-data class Books(val id: Int, val title: String, val author: String) {
-    constructor() : this(-1, "", "")
+data class Books(val id: Int = -1, val title: String = "", val author: String = "")
+
+fun <T> FindOne.As(clazz: Class<T>): T? {
+    return this.`as`(clazz)
 }
 
+fun <T>  Find.As(clazz: Class<T>): MongoCursor<T>? {
+    return this.`as`(clazz)
+}
 
 fun main(args: Array<String>) {
     val db = MongoClient().getDB("api-testing")
@@ -14,8 +22,8 @@ fun main(args: Array<String>) {
 
     val books = jongo.getCollection("requests")
 
-    val all = books.find("{author: 'King'}").`as`(Books::class.java)
-    val one = books.findOne("{author: 'Yes'}").`as`(Books::class.java)
-    all.forEach { println(it) }
+    val all = books.find("{author: 'King'}").As(Books::class.java)
+    val one = books.findOne("{author: 'Yes'}").As(Books::class.java)
+    all?.forEach { println(it) }
     println(one)
 }
