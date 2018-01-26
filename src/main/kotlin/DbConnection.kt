1417fd16d8c37917f483066052db2033965f1bb7
Oleksandr_Pochernin <Oleksandr_Pochernin@epam.com>
2018-01-26 11:51:14 +0200

    [OP] Restructuring functions, small updates in function signatures

--- src/main/kotlin/DbConnection.kt
+++ src/main/kotlin/DbConnection.kt
@@ -1,60 +1,46 @@
 import com.mongodb.*
 
 fun main(args: Array<String>) {
-    createMongoObject(
-            mapOf(
-                    "id" to "18.0",
-                    "title" to "faust1",
-                    "author" to "Gete2",
-                    "array" to arrayOf(1, 2, 3),
-                    "new Object" to createMongoObject(
-                            mapOf(
-                                    "inside object id" to "20",
-                                    "inside object title" to "Some Title"
+    val mongoCollection = setupMongoConnection("localhost", 27017)
+            .getMongoDb("api-testing")
+            .getCollection("requests")
+
+    mongoCollection.insertElements(
+            createMongoObject(
+                    mapOf(
+                            "id" to "18.0",
+                            "title" to "faust1",
+                            "author" to "Gete2",
+                            "array" to arrayOf(1, 2, 3),
+                            "new Object" to createMongoObject(
+                                    mapOf(
+                                            "inside object id" to "20",
+                                            "inside object title" to "Some Title"
+                                    )
                             )
                     )
             )
-    ).insertElements()
+    )
 
+    //get element using map
+    val cursorMap = mongoCollection.getDBCursor(mapOf("array" to arrayOf(1, 2, 3)))
 
-    //get element
-    val cursorMap = getMongoDB()
-            .getCollection("requests")
-            .getDBCursor(mapOf("array" to arrayOf(1, 2, 3)))
-
+    //get element using object
     val obj = createMongoObject(mapOf("array" to arrayOf(1, 2, 3)))
-
-    val cursorObject = getMongoDB()
-            .getCollection("requests")
+    val cursorObject = mongoCollection
             .getDBCursor(obj)
 
-    val wtfMap = cursorMap.one().get("array")
-    val wtfObject = cursorObject.one().get("array")
-
-    val wtfArray = arrayOf(wtfMap)
-
-    println()
 }
 
-private fun getMongoDB(): DB {
-    return MongoClient("localhost", 27017)
-            .getDB("api-testing")
+private fun setupMongoConnection(host: String, port: Int): MongoClient = MongoClient(host, port)
 
-}
+private fun MongoClient.getMongoDb(dbname: String): DB = getDB(dbname)
 
-private fun DB.getCollectionFromDB(collectionName: String): DBCollection {
-    return this.getCollection(collectionName)
-}
+fun DBCollection.getDBCursor(query: BasicDBObject): DBCursor? = this.find(query)
 
-fun BasicDBObject.insertElements() {
-    getMongoDB()
-            .getCollectionFromDB("requests")
-            .insert(this)
-}
+fun <T> DBCollection.getDBCursor(properties: Map<String, T>): DBCursor? = this.find(createMongoObject(properties))
 
-fun DBCollection.getDBCursor(query: BasicDBObject): DBCursor = this.find(query)
-
-fun <T> DBCollection.getDBCursor(properties: Map<String, T>): DBCursor = this.find(createMongoObject(properties))
+fun DBCollection.insertElements(elements: BasicDBObject): WriteResult = this.insert(elements)
 
 private fun <T> createMongoObject(properties: Map<String, T>): BasicDBObject {
     val obj = BasicDBObject()
